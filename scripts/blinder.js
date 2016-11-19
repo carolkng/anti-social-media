1bb7846b3fbab69cd411da450a1ebeec007bfd8c
Carol Ng <ng.km.carol@gmail.com>
2016-11-19 10:20:47 -0800

    V1.0: Everything works

--- scripts/blinder.js
+++ scripts/blinder.js
@@ -1,35 +1,37 @@
 function tryMe() {
   if (document.getElementsByClassName("_48e9").length < 1) {
-    console.log('heyo');
     window.requestAnimationFrame(tryMe);
   } else {
-    console.log("ran");
     startBlock();
   }
-};
+}
 tryMe();
 
 function startBlock() {
-  console.log("rupssate");
-  // !!!
-  // I'M GOING TO BED NOW BUT THIS PART DOESN'T RUN
   chrome.storage.sync.get("blind_messenger", function(data) {
-    if (data["blind_messenger"]) {
-      var list = document.getElementsByClassName("_48e9")[0].getElementsByClassName("_2il3");
+  if (data["blind_messenger"]) {
       chrome.storage.sync.get("blind_names", function(data) {
+        var list = document.getElementsByClassName("_48e9")[0].getElementsByClassName("_2il3");
+        data = data["blind_names"].map(function(ob) {return ob.toLowerCase()});
+        var data_href;
         var i;
         var j;
         for (i=0; i < data.length; i++) {
           for (j=0; j < list.length; j++) {
-            if (list[j].getAttribute("data-href") == "https://www.messenger.com/t/" + data[i] ||
-                list[j].getAttribute("data-href") == "http://www.messenger.com/t/" + data[i]) {
+            data_href = list[j].getAttribute("data-href").toLowerCase();
+            console.log("inner loop");
+            console.log(data_href);
+            console.log("https://www.messenger.com/t/" + data[i]);
+            if (data_href === "https://www.messenger.com/t/" + data[i] ||
+                data_href === "http://www.messenger.com/t/" + data[i]) {
+              console.log("name to be removed:");
               console.log(list[j]);
               list[j].parentElement.parentElement.remove();
               break;
             }
           }
         }
-      }); 
+      });
     }
   });
-}  
+}
--- scripts/popup_settings.js
+++ scripts/popup_settings.js
@@ -2,21 +2,31 @@ document.addEventListener('DOMContentLoaded', function() {
   var input = document.getElementById('blind-option');
   var names = document.getElementById('blinded');
   var button = document.getElementById('blinded_save');
-  
+
   chrome.storage.sync.get("blind_messenger", function(data){
     input.checked = data["blind_messenger"];
   });
-  
+
   chrome.storage.sync.get("blind_names", function(data) {
     names.value = data["blind_names"].join("\n");
   });
-  
+
   input.addEventListener("change", function() {
     chrome.storage.sync.set({"blind_messenger": input.checked});
   }, false);
-  
+
   button.addEventListener("click", function() {
-    chrome.storage.sync.set({"blind_names": names.value.split("\n")}, function() {  
+    var escaped = false;
+    var escaped_names = names.value.split("\n").map(function(ob) {
+      escaped = true;
+      console.log(ob);
+      var rob = ob.replace(/[^0-9a-zA-Z.]/gi, '');
+      return rob;
+    }).filter(function(ob){ return ob.length > 1; });
+    chrome.storage.sync.set({"blind_names": escaped_names}, function() {
+      if (escaped) {
+        names.value = escaped_names.join("\n");
+      }
       button.innerHTML = "Saved!";
       button.disabled = true;
       setTimeout(function() {
@@ -25,6 +35,5 @@ document.addEventListener('DOMContentLoaded', function() {
       }, 1500);
     });
   }, false);
-  
-  console.log(button);
+
 });
